ccc9691c89bd5826f5b65013ab44d7b9a93b790b
ElliotCorazon <lasseyelliot86@gmail.com>
2022-12-24 00:15:30 +0000

    Update 2 files
    I also fixed the bug that was causing my negative values to show when my saving goals are bigger than my income

--- src/planMain.jsx
+++ src/planMain.jsx
@@ -6,7 +6,10 @@ import PlanInput from "./planInput"
     let dailySavings
     let weeklySavings
     let fortnightlySavings
-
+    let amountToSpendDaily_1
+    let amountToSpendWeekly_1
+    let amountToSpendFortnight_1
+    let monthly___costs
 function PlanMain(){
 
   const [income,setIncome] = useState("")
@@ -16,16 +19,25 @@ function PlanMain(){
   const [weeklyCost, setWeeklyCost] = useState("")
   const [fortnightCost, setFortnightCost] = useState("")
   const [showResults, setShowResults] = useState(false)
+  const [selectedNoCheckbox, setSelectedNoCheckbox] = useState(false)
+  const [dailyCost_1,setDailyCost_1] = useState("")
+  const [weeklyCost_1,setWeeklyCost_1] = useState("")
+  const [fortnightCost_1,setFortnightCost_1] = useState("")
 /*I need a state variable to track if the income is greater than the saving
 goal so I can display different data and html 
 */
 const [savingGoalisBiggerThanIncome, setSavingGoalisBiggerThanIncome] = useState(true)
 
+
+//I need a state variable that will display a certain message
+//when the user doesn't select any checkbox value 
+
+
+/*It should be something like, Please select a value  */
   //I need to define the functions that go to take all the data from the child component
 
 function getChildData(monthly_income, saving_goal, checkbox_values){
-    setIncome(monthly_income)
-    setSavingGoal(saving_goal)
+  
     // setCheckBoxValues(checkbox_values)
   //anytime the button is clicked before updating any state, 
   //I want to set all those states that I use their truthy or falsy
@@ -36,11 +48,32 @@ function getChildData(monthly_income, saving_goal, checkbox_values){
   setDailyCost("")
   setWeeklyCost("")
   setFortnightCost("")
+  setDailyCost_1("")
+  setWeeklyCost_1("")
+  setFortnightCost_1("")
 
 
-    if(monthly_income > saving_goal){
+//effort to find bugs in my code.
+console.log("monthly income ",monthly_income )
+console.log("saving goal", saving_goal)
+console.log(typeof monthly_income)
+console.log(typeof saving_goal)
+console.log(monthly_income>saving_goal)
+
+const monthly__income = parseInt(monthly_income)
+const saving__goal = parseInt(saving_goal)
+
+console.log(typeof monthly__income)
+console.log(typeof saving__goal)
+
+  setIncome(monthly__income)
+  setSavingGoal(saving__goal)
+//after logging all the values, it turns out the values I was getting 
+//as monthly income and saving goal were strings and not integers
+//so I had to convert them into integers
+    if(monthly__income > saving__goal){
       
-      const costs = monthly_income - saving_goal
+      const costs = monthly__income - saving__goal
       const amountToSpendDaily = Math.floor(costs/30)
       const amountToSpendWeekly = Math.floor(costs/4)
       const amountToSpendFortnight = Math.floor(costs/2)
@@ -70,11 +103,36 @@ function getChildData(monthly_income, saving_goal, checkbox_values){
   weeklySavings = Math.round((monthlySavings / 4) * 10) / 10;
   fortnightlySavings = Math.round((monthlySavings / 2) * 10) / 10;
 
-    setSavingGoalisBiggerThanIncome(true)
+    setSavingGoalisBiggerThanIncome(!savingGoalisBiggerThanIncome)
     console.log(monthlySavings)
     console.log(dailySavings)
     console.log(weeklySavings)
     console.log(fortnightlySavings)
+  }
+  //if the length of checkbox_values is 0, setSelectedNoCheckbox to true
+  if(checkbox_values.length===0){
+   setSelectedNoCheckbox(true) 
+  }
+  
+  //I also need to display the amount to spend a day or a week or a fortnight
+  //to reach the saving goal even if the saving goal is bigger than the user's income
+  const monthly__costs = Math.floor(monthly__income - monthlySavings)
+  monthly___costs = monthly__costs 
+ amountToSpendDaily_1 = Math.floor(monthly__costs/30)
+  amountToSpendWeekly_1 = Math.floor(monthly__costs/4)
+  amountToSpendFortnight_1 = Math.floor(monthly__costs/2)
+  
+  console.log(amountToSpendDaily_1)
+  console.log(amountToSpendWeekly_1)
+  console.log(amountToSpendFortnight_1)
+  if(checkbox_values.includes("daily")){
+        setDailyCost_1(amountToSpendDaily_1)
+  }
+  if(checkbox_values.includes("weekly")){
+        setWeeklyCost_1(amountToSpendWeekly_1)
+  }
+  if(checkbox_values.includes("fortnight")){
+      setFortnightCost_1(amountToSpendFortnight_1)
   }
   //after all the calculations show the results
   setShowResults(true)
@@ -128,7 +186,7 @@ function getChildData(monthly_income, saving_goal, checkbox_values){
                             className="form-control"
                             id="disabledInput"
                             type="text"
-                            value={`${dailyCost}.00`}
+                            value={`$${dailyCost}.00`}
                             disabled
                           />
                         </fieldset>
@@ -143,7 +201,7 @@ function getChildData(monthly_income, saving_goal, checkbox_values){
                             className="form-control"
                             id="disabledInput"
                             type="text"
-                            value={`${weeklyCost}.00`}
+                            value={`$${weeklyCost}.00`}
                             disabled
                           />
                         </fieldset>
@@ -159,7 +217,7 @@ function getChildData(monthly_income, saving_goal, checkbox_values){
                             className="form-control"
                             id="disabledInput"
                             type="text"
-                            value={`${fortnightCost}.00`}
+                            value={`$${fortnightCost}.00`}
                             disabled
                           />
                         </fieldset>
@@ -193,6 +251,42 @@ function getChildData(monthly_income, saving_goal, checkbox_values){
                       <span className ="badge bg-primary rounded-pill">$ {monthlySavings}</span >
                       </li>
                   </ul>
+                  {/*I want to display all the budget plans here 
+                  If the user doen't select a checkbox let him know */}
+                  <>
+                  {selectedNoCheckbox?(
+                    <div className = "alert alert-dismissible alert-primary mt-2">
+                      <strong>Please select a value from the checkbox</strong>
+                    </div>
+                  ):(
+                      <>
+                      <ul className = "list-group mt-2">
+                      <li className = "list-group-item list-group-item-action active">
+                      Amount to spend: 
+                      </li>
+                      {dailyCost_1!==""?(
+                      <li className = "list-group-item d-flex justify-content-between align-items-center"> daily
+                      <span className ="badge bg-primary rounded-pill">{`$${dailyCost_1}`}</span >
+                      </li>
+                      ):null}
+                      {weeklyCost_1!==""?(
+                      <li className = "list-group-item d-flex justify-content-between align-items-center">weekly<span className ="badge bg-primary rounded-pill">{`$${weeklyCost_1}`}
+                      </span>
+                      </li>
+                      ):null}
+                      {fortnightCost_1!==""?(
+                      <li className = "list-group-item d-flex justify-content-between align-items-center">fortnightly<span className ="badge bg-primary rounded-pill">{`$${fortnightCost_1}`}
+                      </span>
+                      </li>                      
+                      ): null
+                      }
+                      <li className = "list-group-item d-flex justify-content-between align-items-center"> monthly
+                      <span className ="badge bg-primary rounded-pill">{`$${monthly___costs}`}</span >
+                      </li>
+                  </ul>
+                      </>                    
+                  )}
+                  </>
                 </div>
                 )}
                   </>
@@ -214,3 +308,9 @@ If the user did not select it, I don't want it to show */
 /* 
 loop through the seasonsArr, if the checkbox arr contains  */
 //if the user doesn't select anything, we need to prompt the user to select something
+
+
+/*
+(dailyCost_1==0 || dailyCost_1!=="")
+(weeklyCost_1 ==0 || weeklyCost_1!=="")
+ (fortnightCost_1==0 || fortnightCost_1!=="")*/
